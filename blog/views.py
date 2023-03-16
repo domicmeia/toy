bb446a5ed69d2ac0b76b6e89424ec0f988eff576
domicmeia <dhdlqks55@gmail.com>
2023-03-17 02:07:02 +0900

    CBV로 블로그 상세 페이지 생성

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
@@ -7,6 +6,9 @@ from .models import Post
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
 
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
