6dbd3e1aa1f9be1f9cee61ddf0181df7d830b39c
domicmeia <dhdlqks55@gmail.com>
2023-03-28 21:36:53 +0900

    new post upadate

--- blog/tests.py
+++ blog/tests.py
@@ -10,7 +10,8 @@ class TestView(TestCase):
         self.client = Client()
         self.user_trump = User.objects.create_user(username='trump', password='somepassword')
         self.user_obama = User.objects.create_user(username='obama', password='somepassword')
-
+        self.user_obama.is_staff = True
+        self.user_obama.save()
         self.category_ps = Category.objects.create(name='ps', slug='ps')
         self.category_일상 = Category.objects.create(name='일상', slug='일상')
 
@@ -187,7 +188,11 @@ class TestView(TestCase):
 
         # 로그인을 한다.
         self.client.login(username='trump', password='somepassword')
+        response = self.client.get('/blog/create_post/')
+        self.assertNotEqual(response.status_code, 200)
 
+        # staff인 obama로 로그인한다.
+        self.client.login(username='obama', password='somepassword')
         response = self.client.get('/blog/create_post/')
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -206,5 +211,5 @@ class TestView(TestCase):
         self.assertEqual(Post.objects.count(), 4)
         last_post = Post.objects.last()
         self.assertEqual(last_post.title, "Post Form 만들기")
-        self.assertEqual(last_post.author.username, 'trump')
+        self.assertEqual(last_post.author.username, 'obama')
 # Create your tests here.
