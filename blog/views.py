01214a047be6ef15d45343bf094f876ed347cb43
domicmeia <dhdlqks55@gmail.com>
2023-03-29 11:49:28 +0900

    댓글 수정 기능 추가

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Post, Category, Tag
-from django.contrib.auth.mixins import  LoginRequiredMixin, UserPassesTestMixin
+from .models import Post, Category, Tag, Comment
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
@@ -153,6 +153,15 @@ def new_comment(request, pk):
     else:
         raise PermissionDenied
 
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
 
 
 # def index(request):
